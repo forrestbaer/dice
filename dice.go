15b88ee2d8e093f63186ec90c2aed53144f408c2
Forrest Baer <gbaer@vailresorts.com>
2017-12-05 18:20:11 -0700

    Removed dependency on govaluate, updated output when multiple sets of dice are rolled

--- dice.go
+++ dice.go
@@ -7,14 +7,14 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/Knetic/govaluate"
 	"github.com/fatih/color"
 )
 
-var diceRegex = regexp.MustCompile(`([1-9]+)d{1}([0-9]+)([+|-]{1}[0-9]+)*$`)
+var diceRegex = regexp.MustCompile(`([0-9]+)d{1}([0-9]+)(([+|-]){1}[0-9]+)*$`)
+var modRegex = regexp.MustCompile(`([+-])([0-9]+)`)
+var multiple = false
 
 func random(min, max int) int {
-	rand.Seed(time.Now().UTC().UnixNano())
 	return rand.Intn(max-min) + min
 }
 
@@ -35,8 +35,9 @@ func rollDice(num int, sides int) int {
 		brown.Printf("\nRolling a %dd%d\n\n", num, sides)
 	}
 
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	for i := 0; i < num; i++ {
-		time.Sleep(200 * time.Millisecond)
 		result := random(1, sides)
 		total = total + result
 		mwrite.Printf("#%03d %v d%d%v %s\n", i+1, color.HiBlackString("-"), sides, color.HiBlackString(":"), magenta(result))
@@ -49,23 +50,28 @@ func getDice(dice string) {
 
 	white := color.New(color.FgHiWhite)
 	subStr := diceRegex.FindStringSubmatch(dice)
+	modStr := modRegex.FindStringSubmatch(subStr[3])
 
 	numRolls, _ := strconv.Atoi(subStr[1])
 	numSides, _ := strconv.Atoi(subStr[2])
 
 	if subStr[3] != "" {
+		rollPlusMod := 0
 		rollTotal := rollDice(numRolls, numSides)
-		parameters := make(map[string]interface{}, 8)
-		mod, _ := strconv.Atoi(subStr[3])
-		parameters["modifier"] = mod
-		parameters["rollTotal"] = rollTotal
+		modAmmount, _ := strconv.Atoi(modStr[2])
+		modifier := modStr[1]
 
-		expression, _ := govaluate.NewEvaluableExpression("(rollTotal + modifier)")
-		result, _ := expression.Evaluate(parameters)
-		white.Printf("\nTotal: %d %s is %.0f\n", rollTotal, subStr[3], result)
+		switch modifier {
+		case "-":
+			rollPlusMod = rollTotal - modAmmount
+		case "+":
+			rollPlusMod = rollTotal + modAmmount
+		}
+
+		white.Printf("\nTotal: %d %s is %d\n\n", rollTotal, subStr[3], rollPlusMod)
 	} else {
 		rollTotal := rollDice(numRolls, numSides)
-		white.Printf("\nTotal: %d\n", rollTotal)
+		white.Printf("\nTotal: %d\n\n", rollTotal)
 	}
 
 }
@@ -73,10 +79,18 @@ func getDice(dice string) {
 func main() {
 
 	args := os.Args[1:]
+	black := color.New(color.FgHiBlack)
+
+	if len(args) > 1 {
+		multiple = true
+	}
 
 	for i := range args {
 		matched := areDiceFormatted(args[i])
 		if matched {
+			if multiple && i > 0 {
+				black.Println("--------------------")
+			}
 			getDice(args[i])
 		}
 	}
